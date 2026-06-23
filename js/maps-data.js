5804f9a17cac90766dc21b7f8e0c6a341d682d03
Cassetu <cassetu@example.com>
2026-06-23 15:13:38 -0700

    Add maps data script and update Minecraft version options in index.html

---
+++ js/maps-data.js
@@ -0,0 +1,26 @@
+window.MAPS_DATABASE = [
+    {
+        id: "map-1",
+        title: "Fake Title 1",
+        description: "Fake description",
+        version: "1.21.11",
+        mode: "solo",
+        tags: ["Minigame"],
+        downloadUrl: "#",
+        images: [
+            "assets/other/trial-chambers-minecraft.jpg"
+        ]
+    },
+    {
+        id: "map-2",
+        title: "Fake Title 2",
+        description: "Fake description",
+        version: "1.20.1",
+        mode: "multiplayer",
+        tags: ["Parkour"],
+        downloadUrl: "#",
+        images: [
+            "assets/other/trial-chambers-minecraft.jpg"
+        ]
+    }
+];
